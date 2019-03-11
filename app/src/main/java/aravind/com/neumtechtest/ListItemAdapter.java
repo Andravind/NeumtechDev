9bcd59a8f44341e5f04da742acdae722c34cba9e
Aravind <aravind.v234@gmail.com>
2019-03-11 10:29:33 -0400

    updating ui

--- app/src/main/java/aravind/com/neumtechtest/ListItemAdapter.java
+++ app/src/main/java/aravind/com/neumtechtest/ListItemAdapter.java
@@ -3,18 +3,23 @@ package aravind.com.neumtechtest;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHolder> {
 
+    private Context mContext;
+
     public ListItemAdapter(Context mContext) {
+        this.mContext = mContext;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.list_child, viewGroup);
+        return new ViewHolder(view);
     }
 
     @Override
